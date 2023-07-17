e2f23d8869891a77e79ea737a6950d4243731f13
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-07-17 19:39:51 +0700

    new portofolio update

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+//navbar-fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+//hamburger menu
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
