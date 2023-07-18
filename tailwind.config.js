646be395fd7d81db2eae82ec138bab096f6980d5
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-07-18 14:37:02 +0700

    add 404

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./404.html"],
   theme: {
     container: {
       center: true,
