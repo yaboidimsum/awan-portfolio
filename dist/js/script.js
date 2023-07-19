484ea51af1e37edac7fcba7e6730087592251e1e
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-07-19 19:41:36 +0700

    update navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,6 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
@@ -18,4 +17,12 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+//Outside hamburger menu click
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
